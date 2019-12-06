5ada98d5095ddf9dda4eeb2b9332762b5c2286c1
jiangxuan <jiangxuan@dxy.cn>
2019-12-06 14:10:54 +0800

    add test

--- e2e/e2e.test.js
+++ e2e/e2e.test.js
@@ -4,7 +4,7 @@ const request = require('supertest')
 const { app, config } = require('../app')
 
 describe('mock server', () => {
-  it('当 configResponse 配置式, 返回配置的响应', async () => {
+  it('当 configResponse 配置式, 返回配置的响应 1', async () => {
     // arrange
     config.configResponse({
       body: {
@@ -18,5 +18,21 @@ describe('mock server', () => {
 
     // assert
     expect(response.body).toEqual({ foo: 'foo' })
+  })
+
+  it('当 configResponse 配置式, 返回配置的响应 2', async () => {
+    // arrange
+    config.configResponse({
+      body: {
+        bar: 'bar'
+      }
+    })
+
+    // act
+    const response = await request(app)
+      .get('/api/v1/images?param=1')
+
+    // assert
+    expect(response.body).toEqual({ bar: 'bar' })
   })
 })
